a9a3703d3270c27048b0e239ea1b722d7be6aeeb
mafelixx <mairagobbet@gmail.com>
2023-07-04 19:53:15 -0300

    First commit front

--- src/app.py
+++ src/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/clientes')
+def hello2():
+    return render_template('index2.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
